2cdff368b559517745e43cd4cbe7fc43273fb3f5
Corey Carter <corey-carter@hotmail.com>
2021-12-16 13:52:35 -0700

    Temp removed rollbar

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require('express');
 const path = require('path');
 
 // include and initialize the rollbar library with your access token
-const Rollbar = require('rollbar')
-const rollbar = new Rollbar({
-  accessToken: '29e6ac8e72234a3ca4c9ae942316115a',
-  captureUncaught: true,
-  captureUnhandledRejections: true,
-})
+// const Rollbar = require('rollbar')
+// const rollbar = new Rollbar({
+//   accessToken: '29e6ac8e72234a3ca4c9ae942316115a',
+//   captureUncaught: true,
+//   captureUnhandledRejections: true,
+// })
 
 const app = express();
 
@@ -15,7 +15,6 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
-    rollbar.info('HTML file served successfully.')
 })
 
 const port = process.env.PORT || 4545
