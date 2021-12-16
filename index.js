aa1fa66cce058c22d19c4f06aff2347be0c5bc90
Corey Carter <corey-carter@hotmail.com>
2021-12-16 14:31:34 -0700

    Added a fake function

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require('express');
 const path = require('path');
 
 // include and initialize the rollbar library with your access token
-// const Rollbar = require('rollbar')
-// const rollbar = new Rollbar({
-//   accessToken: '29e6ac8e72234a3ca4c9ae942316115a',
-//   captureUncaught: true,
-//   captureUnhandledRejections: true,
-// })
+const Rollbar = require('rollbar')
+const rollbar = new Rollbar({
+  accessToken: '29e6ac8e72234a3ca4c9ae942316115a',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
 
 const app = express();
 
@@ -15,7 +15,19 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
+    rollbar.info('HTML file served successfully.')
 })
+
+app.get('/', (req, res) => {
+    try {
+        doesNotExist()
+    } catch (error) {
+        console.error(error)
+    }
+    res.status(200).send()
+})
+
+app.use(rollbar.errorHandler());
 
 const port = process.env.PORT || 4545
 
